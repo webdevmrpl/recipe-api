fe132c907fb231d16999c9dd460acbf52d032567
webdevmrpl <68972753+webdevmrpl@users.noreply.github.com>
2024-03-26 21:25:03 +0100

    1

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
