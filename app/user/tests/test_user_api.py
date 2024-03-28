12d7864c9706057567378bfc21036ccfa4d74a9f
webdevmrpl <68972753+webdevmrpl@users.noreply.github.com>
2024-03-28 14:25:11 +0100

    Update test_user_api.py

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -46,13 +46,14 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         payload = {
-            'email': 'test1@example.com',
-            'password': 'testp',
+            'email': 'test@example.com',
+            'password': 'pw',
             'name': 'Test Name'
         }
-        create_user(**payload)
+
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']).exists()
         self.assertFalse(user_exists)
